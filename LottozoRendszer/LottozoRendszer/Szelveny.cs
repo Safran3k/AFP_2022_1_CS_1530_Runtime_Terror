97e0b814de18ec3b80dfd8ea71bcac2d8f837b0a
safran3k <futo.ricsi@gmail.com>
2022-10-10 18:10:59 +0200

    Gombok tulajdonságai, elhelyezkedéseinek beállítása

--- LottozoRendszer/LottozoRendszer/Szelveny.cs
+++ LottozoRendszer/LottozoRendszer/Szelveny.cs
@@ -19,7 +19,35 @@ namespace LottozoRendszer
 
         private void Szelveny_Load(object sender, EventArgs e)
         {
-            
+            Button gomb;
+            int top = flowLayoutPanel1.Top + 70;
+            int left = flowLayoutPanel1.Right + 30;
+            int szelesseg = 45;
+            int magassag = 40;
+
+            for (int i = 1; i <= 90; i++)
+            {
+                gomb = new Button();
+                gomb.Name = "szamGombok";
+                gomb.Top = top;
+                gomb.Width = szelesseg;
+                gomb.Left = left;
+                gomb.Height = magassag;
+                gomb.ForeColor = Color.Black;
+                gomb.Text = i.ToString();
+                gomb.Font = new Font("Cooper Black", 15);
+                Controls.Add(gomb);
+
+                if (i % 10 == 0)
+                {
+                    top += magassag + 5;
+                    left = flowLayoutPanel1.Right + 30;
+                }
+                else
+                {
+                    left += szelesseg + 5;
+                }
+            }
         }
     }
 }
