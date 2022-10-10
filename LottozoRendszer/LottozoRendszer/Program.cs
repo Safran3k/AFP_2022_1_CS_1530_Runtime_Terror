de23ba206f861ad84681fb6eace5ea80c63a2cfc
turmxhun <davidbiesz13@gmail.com>
2022-10-10 16:25:58 +0200

    Design hotfix
    Az előző commit hibákkal volt feltöltve, azok lettek orvosolva

--- LottozoRendszer/LottozoRendszer/Program.cs
+++ LottozoRendszer/LottozoRendszer/Program.cs
@@ -16,7 +16,7 @@ namespace LottozoRendszer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SzValasztoForm());
         }
     }
 }
