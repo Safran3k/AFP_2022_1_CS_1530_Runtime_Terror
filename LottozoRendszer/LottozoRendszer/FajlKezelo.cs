976c815c8110dad7445fb589ca0036d0156b3d81
safran3k <futo.ricsi@gmail.com>
2022-10-11 17:26:30 +0200

    Fájlkezelő osztály field-ek/property-k/konstruktor létrehozása

--- LottozoRendszer/LottozoRendszer/FajlKezelo.cs
+++ LottozoRendszer/LottozoRendszer/FajlKezelo.cs
@@ -9,6 +9,17 @@ namespace LottozoRendszer
 {
     class FajlKezelo
     {
-        
+        string nyeroSzamok;
+        string valasztottSzamok;
+
+        public FajlKezelo()
+        {
+            NyeroSzamok = nyeroSzamok;
+            ValasztottSzamok = valasztottSzamok;
+        }
+
+
+        public string NyeroSzamok { get => nyeroSzamok; set => nyeroSzamok = value; }
+        public string ValasztottSzamok { get => valasztottSzamok; set => valasztottSzamok = value; }
     }
 }
